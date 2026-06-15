3ec6f914ec0bbc1c24ae32781fca75f9ccfca5ca
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 11:33:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+module "resource_groups" {
 
+  source = "./modules/resource-group"
+
+  for_each = var.regions
+
+  name = "rg-${each.key}-${var.environment}"
+
+  location = each.value.location
+
+  tags = local.common_tags
+}
