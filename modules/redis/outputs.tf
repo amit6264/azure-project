2bfa1254478a16e8085941c0a39142aa12a4f036
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 12:53:24 +0530

    Create outputs.tf

---
+++ modules/redis/outputs.tf
@@ -0,0 +1,12 @@
+output "id" {
+  value = azurerm_redis_enterprise_cluster.this.id
+}
+
+output "name" {
+  value = azurerm_redis_enterprise_cluster.this.name
+}
+
+output "hostname" {
+  value =
+  azurerm_redis_enterprise_cluster.this.hostname
+}
