acfeb48b030631f948331098f4dc50a9b97b11c3
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 13:56:44 +0530

    Create outputs.tf

---
+++ modules/frontdoor/outputs.tf
@@ -0,0 +1,7 @@
+output "frontdoor_profile_id" {
+  value = azurerm_cdn_frontdoor_profile.this.id
+}
+
+output "frontdoor_endpoint_hostname" {
+  value = azurerm_cdn_frontdoor_endpoint.this.host_name
+}
