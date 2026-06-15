8b6172c76c2a470cf246f932d364dc792ef05e6f
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 13:13:49 +0530

    Create outputs.tf

---
+++ modules/firewall/outputs.tf
@@ -0,0 +1,13 @@
+output "id" {
+  value = azurerm_firewall.this.id
+}
+
+output "private_ip" {
+  value =
+  azurerm_firewall.this.ip_configuration[0].private_ip_address
+}
+
+output "policy_id" {
+  value =
+  azurerm_firewall_policy.this.id
+}
