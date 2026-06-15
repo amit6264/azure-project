fd2766910a3c4d45d202d1ac845ef099778341fa
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 11:38:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,34 @@ module "resource_groups" {
 
   tags = local.common_tags
 }
+
+
+module "network" {
+
+  source = "./modules/network"
+
+  for_each = var.regions
+
+  resource_group_name =
+  module.resource_groups[each.key].name
+
+  location = each.value.location
+
+  vnet_name = "vnet-${each.key}-${var.environment}"
+
+  vnet_cidr = each.value.vnet_cidr
+
+  aks_subnet_cidr =
+  each.value.aks_subnet_cidr
+
+  pe_subnet_cidr =
+  each.value.pe_subnet_cidr
+
+  firewall_subnet_cidr =
+  each.value.firewall_subnet_cidr
+
+  appgw_subnet_cidr =
+  each.value.appgw_subnet_cidr
+
+  tags = local.common_tags
+}
