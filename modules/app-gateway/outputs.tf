a4bebc3f952bce3b710a4513aee18169f57cbffc
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 13:52:07 +0530

    Create outputs.tf

---
+++ modules/app-gateway/outputs.tf
@@ -0,0 +1,13 @@
+output "id" {
+  value = azurerm_application_gateway.this.id
+}
+
+output "frontend_ip" {
+  value =
+  azurerm_public_ip.this.ip_address
+}
+
+output "waf_policy_id" {
+  value =
+  azurerm_web_application_firewall_policy.this.id
+}
