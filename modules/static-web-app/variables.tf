a9af57dd7b460ce8f15b64c862b2aa86516c6c93
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 12:20:16 +0530

    Create variables.tf

---
+++ modules/static-web-app/variables.tf
@@ -0,0 +1,25 @@
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "sku_tier" {
+  type    = string
+  default = "Standard"
+}
+
+variable "sku_size" {
+  type    = string
+  default = "Standard"
+}
+
+variable "tags" {
+  type = map(string)
+}
