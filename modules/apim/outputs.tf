d69b7ab808465a418573b3ed0a397184e0004242
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 12:18:16 +0530

    Create outputs.tf

---
+++ modules/apim/outputs.tf
@@ -0,0 +1,16 @@
+output "id" {
+  value = azurerm_api_management.this.id
+}
+
+output "name" {
+  value = azurerm_api_management.this.name
+}
+
+output "gateway_url" {
+  value = azurerm_api_management.this.gateway_url
+}
+
+output "principal_id" {
+  value =
+  azurerm_api_management.this.identity[0].principal_id
+}
