60f7329cd50f14bb7d11170764a6b76c973b9572
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 12:52:51 +0530

    Update main.tf

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -14,3 +14,50 @@ resource "azurerm_redis_enterprise_cluster" "this" {
 
   tags = var.tags
 }
+
+
+
+
+resource "azurerm_redis_enterprise_database" "this" {
+
+  name = "default"
+
+  cluster_id =
+  azurerm_redis_enterprise_cluster.this.id
+
+  client_protocol = "Encrypted"
+
+  clustering_policy = "EnterpriseCluster"
+}
+
+
+
+
+resource "azurerm_private_endpoint" "this" {
+
+  name = "${var.name}-pe"
+
+  location = var.location
+
+  resource_group_name =
+  var.resource_group_name
+
+  subnet_id =
+  var.private_endpoint_subnet_id
+
+  private_service_connection {
+
+    name = "${var.name}-psc"
+
+    private_connection_resource_id =
+    azurerm_redis_enterprise_cluster.this.id
+
+    subresource_names = [
+      "redisEnterprise"
+    ]
+
+    is_manual_connection = false
+  }
+
+  tags = var.tags
+}
