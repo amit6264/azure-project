7b895622283f53c42cb68091ebffe7eac9abd0f2
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 11:29:35 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "environment" {
+  type = string
+}
+
+variable "regions" {
+
+  type = map(object({
+
+    location = string
+
+    vnet_cidr = string
+
+    aks_subnet_cidr = string
+
+    pe_subnet_cidr = string
+
+    firewall_subnet_cidr = string
+
+    appgw_subnet_cidr = string
+
+  }))
+}
