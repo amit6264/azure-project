4657d95709a9ef23a8b652f6169bb9d31dd62312
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 12:08:36 +0530

    Create variables.tf

---
+++ modules/keyvault/variables.tf
@@ -0,0 +1,27 @@
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "private_endpoint_subnet_id" {
+  type = string
+}
+
+variable "keyvault_private_dns_zone_id" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
