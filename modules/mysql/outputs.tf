f3b49dd6bf1cd4351f1e3b518fbcad908f494fa5
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 12:41:56 +0530

    Create outputs.tf

---
+++ modules/mysql/outputs.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = azurerm_mysql_flexible_server.this.id
+}
+
+output "fqdn" {
+  value = azurerm_mysql_flexible_server.this.fqdn
+}
+
+output "name" {
+  value = azurerm_mysql_flexible_server.this.name
+}
