02bb8b5206aa7217cffb2d4b9c709edcdf666930
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 14:12:30 +0530

    Create outputs.tf

---
+++ modules/aks/outputs.tf
@@ -0,0 +1,17 @@
+output "id" {
+  value = azurerm_kubernetes_cluster.this.id
+}
+
+output "name" {
+  value = azurerm_kubernetes_cluster.this.name
+}
+
+output "kubelet_identity_object_id" {
+  value =
+  azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+}
+
+output "oidc_issuer_url" {
+  value =
+  azurerm_kubernetes_cluster.this.oidc_issuer_url
+}
