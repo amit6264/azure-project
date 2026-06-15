56eeac51d33cd88383b58e7a299610b27e51dea4
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-15 11:37:59 +0530

    Create outputs.tf

---
+++ modules/network/outputs.tf
@@ -0,0 +1,23 @@
+output "vnet_id" {
+  value = azurerm_virtual_network.this.id
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.this.name
+}
+
+output "aks_subnet_id" {
+  value = azurerm_subnet.aks.id
+}
+
+output "private_endpoint_subnet_id" {
+  value = azurerm_subnet.private_endpoint.id
+}
+
+output "firewall_subnet_id" {
+  value = azurerm_subnet.firewall.id
+}
+
+output "appgw_subnet_id" {
+  value = azurerm_subnet.appgw.id
+}
